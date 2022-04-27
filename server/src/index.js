4b9707ac0d927ef0b598010f2a817d29d0fcabb3
Andrei Calinescu <andrei@lunit.io>
2022-04-27 15:36:33 +0900

    feat: add resolves + remove mocks

--- server/src/index.js
+++ server/src/index.js
@@ -1,31 +1,18 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
-
-const mocks = {
-  Query: () => ({
-    tracksForHome: () => [...new Array(9)],
-  }),
-  Track: () => ({
-    id: () => 'track_01',
-    title: () => 'Astro Kitty, Space Explorer',
-    author: () => {
-      return {
-        name: 'Grumpy Cat',
-        photo: 'https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg',
-      };
-    },
-    thumbnail: () => 'https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg',
-    length: () => 1210,
-    modulesCount: () => 6,
-  }),
-};
+const resolvers = require('./resolvers');
+const TrackAPI = require('./datasources/track-api');
 
 const server = new ApolloServer({
   typeDefs,
-  mocks,
+  resolvers,
+  dataSources: () => ({
+    trackAPI: new TrackAPI(),
+  }),
 });
 
 server.listen().then(() => {
+  // eslint-disable-next-line no-console
   console.log(`
     🚀  Server is running!
     🔉  Listening on port 4000
